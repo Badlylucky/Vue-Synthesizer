0a349c4da34f206116a06409088155a393efa636
Badlylucky <u770518g@ecs.osaka-u.ac.jp>
2020-09-16 16:22:03 +0900

    全体をコンポーネント化

--- index.js
+++ index.js
@@ -25,115 +25,247 @@ function init() {
 		API.LMSFinish("");
 	}
 }
-//引数のmidiノート番号の周波数を計算する
-function calcFrequency(note) {
-	let basefreq
-	const n = (note + 12) % 12
-	let basenote
-	switch (n) {
-		case 0:
-			basefreq = 261.6
-			basenote = 60
-			break
-		case 1:
-			basefreq = 277.2
-			basenote = 61
-			break
-		case 2:
-			basefreq = 293.7
-			basenote = 62
-			break
-		case 3:
-			basefreq = 311.1
-			basenote = 63
-			break
-		case 4:
-			basefreq = 329.6
-			basenote = 64
-			break
-		case 5:
-			basefreq = 349.2
-			basenote = 65
-			break
-		case 6:
-			basefreq = 370.0
-			basenote = 66
-			break
-		case 7:
-			basefreq = 392.0
-			basenote = 67
-			break
-		case 8:
-			basefreq = 415.3
-			basenote = 68
-			break
-		case 9:
-			basefreq = 440.0
-			basenote = 69
-			break
-		case 10:
-			basefreq = 466.2
-			basenote = 70
-			break
-		case 11:
-			basefreq = 493.9
-			basenote = 71
-			break
-	}
-	return basefreq * Math.pow(2.0, (note - basenote) / 12)
-}
-var app = new Vue({
-	el: '#app',
-	data: {
-		audioContext: [],
-		//Wave 1
-		mainosc: [],
-		mainWave: "sine",
-		mainGain: 350,
-		mainDetune: 0,
-		mainOctave: 4,
-		mainOscVolume: [],
-		//Wave 2
-		subosc: [],
-		subWave: "sine",
-		subGain: 350,
-		subDetune: 0,
-		subOctave: 4,
-		subOscVolume: [],
-		//Filter
-		LPF: [],
-		LPFCutoff: 15000,
-		LPFEmphasis: 5,
-		HPF: [],
-		HPFCutoff: 50,
-		HPFEmphasis: 5,
-		//LFO 1
-		mainLFO: [],
-		mainLFOWave: "sine",
-		mainLFORate: 5,
-		mainLFOSync: false,
-		mainLFOApply: [],
-		mainLFOGain: [],
-		mainLFODepth: 10,
-		//LFO 2
-		subLFO: [],
-		subLFOWave: "sine",
-		subLFORate: 5,
-		subLFOSync: false,
-		subLFOApply: [],
-		subLFOGain: [],
-		subLFODepth: 10,
-		//Env
-		Envelope: [],
-		attack: 0.5,
-		attackCurve: 0.0,
-		decay: 0.5,
-		sustain: 0.5,
-		release: 0.5,
-		//Others
-		masterVolume: [],
-		nowplaying: false,
-		note: 72
+Vue.component('vue-synthesizer', {
+	template: `<div v-on:mouseup="keyStop()">
+	<div class="synth">
+		<!--オシレーター部-->
+		<main style="display: flex">
+			<div class="half-box">
+				<span>Wave 1</span>
+				<br>
+				<input type="radio" value="sine" v-model="mainWave">Sine
+				<input type="radio" value="square" v-model="mainWave">Square
+				<input type="radio" value="sawtooth" v-model="mainWave">Sawtooth
+				<input type="radio" value="triangle" v-model="mainWave">Triangle
+				<br>
+				<span>Octave</span><input type="tel" class="text-small"
+					v-on:blur="function(){mainOctave=clampInput(mainOctave,-3,11,4)}" v-model="mainOctave">
+				<span>Pitch</span><input type="tel" class="text-mid"
+					v-on:blur="function(){mainDetune=clampInput(mainDetune,-153600,153600,0)}" v-model="mainDetune">
+				<input type="range" min="-200" max="200" v-model="mainDetune">
+				<br>
+				<span>Volume</span><input type="range" min="0" max="500" v-model="mainGain">
+			</div>
+			<div class="half-box">
+				<span>Wave 2</span>
+				<br>
+				<input type="radio" value="sine" v-model="subWave">Sine
+				<input type="radio" value="square" v-model="subWave">Square
+				<input type="radio" value="sawtooth" v-model="subWave">Sawtooth
+				<input type="radio" value="triangle" v-model="subWave">Triangle
+				<br>
+				<span>Octave</span><input type="tel" class="text-small"
+					v-on:blur="function(){subOctave=clampInput(subOctave,-3,11,4)}" v-model="subOctave">
+				<span>Pitch</span><input type="tel" class="text-mid"
+					v-on:blur="function(){subDetune=clampInput(subDetune,-153600,153600,0)}" v-model="subDetune">
+				<input type="range" min="-200" max="200" v-model="subDetune">
+				<br>
+				<span>Volume</span><input type="range" min="0" max="500" v-model="subGain">
+			</div>
+		</main>
+		<!--フィルター部-->
+		<main class="full-box">
+			<span>Filter(LPF & HPF)</span>
+			<br>
+			<main style="display: flex">
+				<div>
+					<span>Low Pass</span>
+					<br>
+					<span>Cutoff</span><input type="tel" class="text-large"
+						v-on:blur="function(){LPFCutoff=clampInput(LPFCutoff,0,24000,15000)}" v-model="LPFCutoff">
+					<input type="range" min="0" max="17000" v-model="LPFCutoff">
+					<br>
+					<span>Emphasis</span><input type="tel" class="text-mid"
+						v-on:blur="function(){LPFEmphasis=clampInput(LPFEmphasis,0,30,5)}" v-model="LPFEmphasis">
+					<input type="range" min="0" max="20" step="0.1" v-model="LPFEmphasis">
+				</div>
+				<div>
+					<span>High Pass</span>
+					<br>
+					<span>Cutoff</span><input type="tel" class="text-large"
+						v-on:blur="function(){HPFCutoff=clampInput(HPFCutoff,0,24000,50)}" v-model="HPFCutoff">
+					<input type="range" min="0" max="17000" v-model="HPFCutoff">
+					<br>
+					<span>Emphasis</span><input type="tel" class="text-mid"
+						v-on:blur="function(){HPFEmphasis=clampInput(HPFEmphasis,0,30,5)}" v-model="HPFEmphasis">
+					<input type="range" min="0" max="20" step="0.1" v-model="HPFEmphasis">
+				</div>
+			</main>
+		</main>
+		<!--エンベロープ-->
+		<!--LFO-->
+		<main style="display: flex">
+			<div class="half-box">
+				<span>LFO 1</span>
+				<br>
+				<input type="radio" value="sine" v-model="mainLFOWave">Sine
+				<input type="radio" value="square" v-model="mainLFOWave">Square
+				<input type="radio" value="sawtooth" v-model="mainLFOWave">Sawtooth
+				<input type="radio" value="triangle" v-model="mainLFOWave">Triangle
+				<br>
+				<span>Rate</span><input type="tel" class="text-large"
+					v-on:blur="function(){mainLFORate=clampInput(mainLFORate,-10000,10000,5)}"
+					v-model="mainLFORate">
+				<input type="range" class="range-small" min="0" max="15" v-model="mainLFORate">
+				<input type="checkbox" v-model="mainLFOSync">Sync.
+				<br>
+				<span>Depth</span><input type="tel" class="text-large"
+					v-on:blur="function(){mainLFODepth=clampInput(mainLFODepth,-1000000,1000000,10)}"
+					v-model="mainLFODepth">
+				<span v-if="mainLFOSync">
+					<input type="range" class="range-large" min="0" max="10000" v-model="mainLFODepth">
+				</span>
+				<span v-else>
+					<input type="range" class="range-large" min="0" max="100" v-model="mainLFODepth">
+				</span>
+				<br>
+				<div style="font-size: 60%;">
+					<input type="checkbox" value="mainOscFreq" v-model="mainLFOApply">Freq.(Wave1)
+					<input type="checkbox" value="mainOscVol" v-model="mainLFOApply">Vol.(Wave1)
+					<input type="checkbox" value="subOscFreq" v-model="mainLFOApply">Freq.(Wave2)
+					<input type="checkbox" value="subOscVol" v-model="mainLFOApply">Vol.(Wave2)
+					<br>
+					<input type="checkbox" value="LPFCutoff" v-model="mainLFOApply">Cutoff(LPF)
+					<input type="checkbox" value="LPFEmphasis" v-model="mainLFOApply">Emp.(LPF)
+					<input type="checkbox" value="HPFCutoff" v-model="mainLFOApply">Cutoff(HPF)
+					<input type="checkbox" value="HPFEmphasis" v-model="mainLFOApply">Emp.(HPF)
+					<br>
+					<input type="checkbox" value="mainLFORate" v-model="mainLFOApply">Rate(LFO1)
+					<input type="checkbox" value="mainLFODepth" v-model="mainLFOApply">Depth(LFO1)
+					<input type="checkbox" value="subLFORate" v-model="mainLFOApply">Rate(LFO2)
+					<input type="checkbox" value="subLFODepth" v-model="mainLFOApply">Depth(LFO2)
+				</div>
+			</div>
+			<div class="half-box">
+				<span>LFO 2</span>
+				<br>
+				<input type="radio" value="sine" v-model="subLFOWave">Sine
+				<input type="radio" value="square" v-model="subLFOWave">Square
+				<input type="radio" value="sawtooth" v-model="subLFOWave">Sawtooth
+				<input type="radio" value="triangle" v-model="subLFOWave">Triangle
+				<br>
+				<span>Rate</span><input type="tel" class="text-large"
+					v-on:blur="function(){subLFORate=clampInput(subLFORate,-10000,10000,5)}" v-model="subLFORate">
+				<input type="range" class="range-small" min="0" max="15" v-model="subLFORate">
+				<input type="checkbox" v-model="subLFOSync">Sync.
+				<br>
+				<span>Depth</span><input type="tel" class="text-large"
+					v-on:blur="function(){subLFODepth=clampInput(subLFODepth,-1000000,1000000,10)}"
+					v-model="subLFODepth">
+				<span v-if="subLFOSync">
+					<input type="range" class="range-large" min="0" max="10000" v-model="subLFODepth">
+				</span>
+				<span v-else>
+					<input type="range" class="range-large" min="0" max="100" v-model="subLFODepth">
+				</span>
+				<br>
+				<div style="font-size:60%">
+					<input type="checkbox" value="mainOscFreq" v-model="subLFOApply">Freq.(Wave1)
+					<input type="checkbox" value="mainOscVol" v-model="subLFOApply">Vol.(Wave1)
+					<input type="checkbox" value="subOscFreq" v-model="subLFOApply">Freq.(Wave2)
+					<input type="checkbox" value="subOscVol" v-model="subLFOApply">Vol.(Wave2)
+					<br>
+					<input type="checkbox" value="LPFCutoff" v-model="subLFOApply">Cutoff(LPF)
+					<input type="checkbox" value="LPFEmphasis" v-model="subLFOApply">Emp.(LPF)
+					<input type="checkbox" value="HPFCutoff" v-model="subLFOApply">Cutoff(HPF)
+					<input type="checkbox" value="HPFEmphasis" v-model="subLFOApply">Emp.(HPF)
+					<br>
+					<input type="checkbox" value="mainLFORate" v-model="subLFOApply">Rate(LFO1)
+					<input type="checkbox" value="mainLFODepth" v-model="subLFOApply">Depth(LFO1)
+					<input type="checkbox" value="subLFORate" v-model="subLFOApply">Rate(LFO2)
+					<input type="checkbox" value="subLFODepth" v-model="subLFOApply">Depth(LFO2)
+				</div>
+			</div>
+		</main>
+		<!--エンベロープ-->
+		<main class="full-box">
+			<span>Envelope(Master Volume)</span>
+			<br>
+			<span>Attack</span><input type="tel" class="text-mid"
+				v-on:blur="function(){attack=clampInput(attack,0.01,60.0,0.5)}" v-model="attack">
+			<input type="range" min="0.01" max="2.0" step="0.01" v-model="attack">
+			<span>Attack Curve</span><input type="tel" class="text-mid"
+				v-on:blur="function(){attackCurve=clampInput(attackCurve,0,1.0,0)}" v-model="attackCurve"><input
+				type="range" min="0" max="1" step="0.01" v-model="attackCurve">
+			<br>
+			<span>Decay</span><input type="tel" class="text-mid"
+				v-on:blur="function(){decay=clampInput(decay,0.01,60.0,0.5)}" v-model="decay"><input type="range"
+				min="0.01" max="2.0" step="0.01" v-model="decay">
+			<span>Sustain</span><input type="tel" class="text-mid"
+				v-on:blur="function(){sustain=clampInput(sustain,0,1.0,0.5)}" v-model="sustain"><input type="range"
+				min="0.0" max="1.0" step="0.01" v-model="sustain">
+			<span>Release</span><input type="tel" class="text-mid"
+				v-on:blur="function(){release=clampInput(release,0.01,60.0,0.5)}" v-model="release"><input
+				type="range" min="0.01" max="2.0" step="0.01" v-model="release">
+		</main>
+		<ul class="set">
+			<li class="white c" v-on:mousedown="keyPlay(60)" v-on:mouseup="keyStop()"></li>
+			<li class="black cs" v-on:mousedown="keyPlay(61)" v-on:mouseup="keyStop()"></li>
+			<li class="white d" v-on:mousedown="keyPlay(62)" v-on:mouseup="keyStop()"></li>
+			<li class="black ds" v-on:mousedown="keyPlay(63)" v-on:mouseup="keyStop()"></li>
+			<li class="white e" v-on:mousedown="keyPlay(64)" v-on:mouseup="keyStop()"></li>
+			<li class="white f" v-on:mousedown="keyPlay(65)" v-on:mouseup="keyStop()"></li>
+			<li class="black fs" v-on:mousedown="keyPlay(66)" v-on:mouseup="keyStop()"></li>
+			<li class="white g" v-on:mousedown="keyPlay(67)" v-on:mouseup="keyStop()"></li>
+			<li class="black gs" v-on:mousedown="keyPlay(68)" v-on:mouseup="keyStop()"></li>
+			<li class="white a" v-on:mousedown="keyPlay(69)" v-on:mouseup="keyStop()"></li>
+			<li class="black as" v-on:mousedown="keyPlay(70)" v-on:mouseup="keyStop()"></li>
+			<li class="white b" v-on:mousedown="keyPlay(71)" v-on:mouseup="keyStop()"></li>
+		</ul>
+	</div>
+</div>`,
+	data: function () {
+		return {
+			audioContext: [],
+			//Wave 1
+			mainosc: [],
+			mainWave: "sine",
+			mainGain: 350,
+			mainDetune: 0,
+			mainOctave: 4,
+			mainOscVolume: [],
+			//Wave 2
+			subosc: [],
+			subWave: "sine",
+			subGain: 350,
+			subDetune: 0,
+			subOctave: 4,
+			subOscVolume: [],
+			//Filter
+			LPF: [],
+			LPFCutoff: 15000,
+			LPFEmphasis: 5,
+			HPF: [],
+			HPFCutoff: 50,
+			HPFEmphasis: 5,
+			//LFO 1
+			mainLFO: [],
+			mainLFOWave: "sine",
+			mainLFORate: 5,
+			mainLFOSync: false,
+			mainLFOApply: [],
+			mainLFOGain: [],
+			mainLFODepth: 10,
+			//LFO 2
+			subLFO: [],
+			subLFOWave: "sine",
+			subLFORate: 5,
+			subLFOSync: false,
+			subLFOApply: [],
+			subLFOGain: [],
+			subLFODepth: 10,
+			//Env
+			Envelope: [],
+			attack: 0.5,
+			attackCurve: 0.0,
+			decay: 0.5,
+			sustain: 0.5,
+			release: 0.5,
+			//Others
+			masterVolume: [],
+			nowplaying: false,
+			note: 72
+		}
 	},
 	methods: {
 		keyPlay: function (note) {
@@ -141,10 +273,10 @@ var app = new Vue({
 			this.mainOscVolume.gain.value = this.mainGain / 1000;
 			this.subOscVolume.gain.value = this.subGain / 1000;
 			this.mainosc.type = this.mainWave;
-			this.mainosc.frequency.value = calcFrequency(note + (this.mainOctave - 4) * 12);
+			this.mainosc.frequency.value = this.calcFrequency(note + (this.mainOctave - 4) * 12);
 			this.mainosc.detune.value = this.mainDetune;
 			this.subosc.type = this.subWave;
-			this.subosc.frequency.value = calcFrequency(note + (this.subOctave - 4) * 12);
+			this.subosc.frequency.value = this.calcFrequency(note + (this.subOctave - 4) * 12);
 			this.subosc.detune.value = this.subDetune;
 			this.LPF.frequency.value = this.LPFCutoff;
 			this.LPF.Q.value = this.LPFEmphasis;
@@ -173,13 +305,13 @@ var app = new Vue({
 			this.mainLFOGain.disconnect();
 			this.subLFOGain.disconnect();
 			if (this.mainLFOSync) {
-				this.mainLFO.frequency.value = calcFrequency(this.note + (this.mainOctave - 4) * 12) * this.mainLFORate;
+				this.mainLFO.frequency.value = this.calcFrequency(this.note + (this.mainOctave - 4) * 12) * this.mainLFORate;
 			} else {
 				this.mainLFO.frequency.value = this.mainLFORate;
 			}
 			this.mainLFOGain.gain.value = this.mainLFODepth;
 			if (this.subLFOSync) {
-				this.subLFO.frequency.value = calcFrequency(this.note + (this.mainOctave - 4) * 12) * this.subLFORate;
+				this.subLFO.frequency.value = this.calcFrequency(this.note + (this.mainOctave - 4) * 12) * this.subLFORate;
 			} else {
 				this.subLFO.frequency.value = this.subLFORate;
 			}
@@ -259,6 +391,63 @@ var app = new Vue({
 				ret = value;
 			}
 			return ret;
+		},
+		//引数のmidiノート番号の周波数を計算する
+		calcFrequency: function (note) {
+			let basefreq;
+			const n = (note + 12) % 12;
+			let basenote;
+			switch (n) {
+				case 0:
+					basefreq = 261.6;
+					basenote = 60;
+					break
+				case 1:
+					basefreq = 277.2;
+					basenote = 61;
+					break
+				case 2:
+					basefreq = 293.7;
+					basenote = 62;
+					break
+				case 3:
+					basefreq = 311.1;
+					basenote = 63;
+					break;
+				case 4:
+					basefreq = 329.6;
+					basenote = 64;
+					break
+				case 5:
+					basefreq = 349.2;
+					basenote = 65;
+					break;
+				case 6:
+					basefreq = 370.0;
+					basenote = 66;
+					break;
+				case 7:
+					basefreq = 392.0;
+					basenote = 67;
+					break;
+				case 8:
+					basefreq = 415.3;
+					basenote = 68;
+					break;
+				case 9:
+					basefreq = 440.0;
+					basenote = 69;
+					break;
+				case 10:
+					basefreq = 466.2;
+					basenote = 70;
+					break;
+				case 11:
+					basefreq = 493.9;
+					basenote = 71;
+					break;
+			}
+			return basefreq * Math.pow(2.0, (note - basenote) / 12);
 		}
 	},
 	created: async function () {
@@ -299,6 +488,9 @@ var app = new Vue({
 		this.mainLFO.stop(0);
 		this.subLFO.stop(0);
 	}
+});
+var app = new Vue({
+	el: '#app'
 })
 function sendResult() {
 	API.LMSSetValue('cmi.core.score.raw', app.score + "");
